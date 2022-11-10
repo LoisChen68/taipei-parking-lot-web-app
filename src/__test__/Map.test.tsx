e6676456611d70c0cdf03ebc58ee175612d62130
LoisChen68 <a8283626@gmail.com>
2022-11-10 11:12:48 +0800

    test: add SearchBar test  & modify Map test

--- src/__test__/Map.test.tsx
+++ src/__test__/Map.test.tsx
@@ -1,8 +1,20 @@
-import { screen, render } from '@testing-library/react'
+import {screen, render} from '@testing-library/react'
 import MapBox from '../components/Map/Map'
 import * as ReactGoogleMapApi from '@react-google-maps/api'
+import {mockNavigatorGeolocation} from '../__mock__/mockNavigatorGeolocation'
 
 describe('Map', () => {
+  const {getCurrentPositionMock} = mockNavigatorGeolocation();
+  getCurrentPositionMock.mockImplementation((success, rejected) =>
+    rejected({
+      code: '',
+      message: '',
+      PERMISSION_DENIED: '',
+      POSITION_UNAVAILABLE: '',
+      TIMEOUT: '',
+    })
+
+  )
   test('should shows loader', () => {
     jest.spyOn(ReactGoogleMapApi, 'useJsApiLoader').mockReturnValue({
       isLoaded: false,
---
+++ src/__test__/Search.test.tsx
@@ -0,0 +1,9 @@
+import {screen, render} from '@testing-library/react'
+import Search from '../components/SearchBar/SearchBar'
+const setPlace = jest.fn()
+
+test('should render Search component', () => {
+  render(<Search setPlace={setPlace} />)
+
+  expect(screen.getByPlaceholderText(/輸入你想去的地點/i)).toBeInTheDocument
+})
