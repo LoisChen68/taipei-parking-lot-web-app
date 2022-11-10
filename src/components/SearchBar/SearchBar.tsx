503ff34473322664bdfd1ef14a551ff48872e42c
LoisChen68 <a8283626@gmail.com>
2022-11-10 11:39:41 +0800

    feat: add clear search input value & modify search css

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -12,18 +12,18 @@ import usePlacesAutocomplete, {
   getLatLng,
 } from 'use-places-autocomplete'
 import style from './SearchBar.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faMagnifyingGlass, faXmark} from '@fortawesome/free-solid-svg-icons'
 
 type PlacesProps = {
   setPlace: (position: google.maps.LatLngLiteral) => void
 }
 
-export default function Navbar({ setPlace }: PlacesProps) {
+export default function Navbar({setPlace}: PlacesProps) {
   const {
     value,
     setValue,
-    suggestions: { status, data },
+    suggestions: {status, data},
     clearSuggestions,
   } = usePlacesAutocomplete()
 
@@ -31,10 +31,11 @@ export default function Navbar({ setPlace }: PlacesProps) {
     setValue(val, false)
     clearSuggestions()
 
-    const results = await getGeocode({ address: val })
-    const { lat, lng } = getLatLng(results[0])
-    setPlace({ lat, lng })
+    const results = await getGeocode({address: val})
+    const {lat, lng} = getLatLng(results[0])
+    setPlace({lat, lng})
   }
+
   return (
     <div className={style.bar}>
       <Combobox onSelect={handleSelect} className={style['search-bar']}>
@@ -44,12 +45,13 @@ export default function Navbar({ setPlace }: PlacesProps) {
           className={style['combobox-input']}
           placeholder="輸入你想去的地點"
         ></ComboboxInput>
-        <FontAwesomeIcon className={style.icon} icon={faMagnifyingGlass} />
+        <FontAwesomeIcon className={style.xIcon} icon={faXmark} onClick={() => setValue('')} />
+        <FontAwesomeIcon className={style.searchIcon} icon={faMagnifyingGlass} />
         <ComboboxPopover>
           {/* 顯示地點清單 */}
           <ComboboxList>
             {status === 'OK' &&
-              data.map(({ place_id, description }) => (
+              data.map(({place_id, description}) => (
                 <ComboboxOption key={place_id} value={description} />
               ))}
           </ComboboxList>
