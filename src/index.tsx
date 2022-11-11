fea63fbf7f85b6fd81eefe1bb296c0ac4a394e1a
LoisChen68 <a8283626@gmail.com>
2022-11-11 10:11:36 +0800

    chore: setup openreplay

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,13 @@ import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals'
 import {BrowserRouter} from 'react-router-dom'
+import Tracker from '@openreplay/tracker'
+
+
+const tracker = new Tracker({
+  projectKey: process.env.REACT_APP_PROJECT_KEY || '',
+});
+tracker.start();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const BASE_URL = 'taipei-parking-lot-web-app'
@@ -16,6 +23,8 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 )
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
